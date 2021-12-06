c6d070b8ea9abb8fceb9bbb52f92863a454b7cc4
SHUBHAM VERMA <shubhamverma2604@gmail.com>
2021-12-06 15:53:52 +0530

    Update collegeController.js

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -15,43 +15,62 @@ const createColleges = async function (req, res) {
 
     try {
         const requestBody = req.body;
-
-        if(!isValidRequestBody(requestBody)) {
-            res.status(400).send({status: false, message: 'Invalid request parameters. Please provide college details'})
-            return
-        }
-
-        // Extract params
-        const {name, fullName, logo } = requestBody;
-
-
-        // Validation starts
-        if(!isValid(name)) {
+        let temp = requestBody.name
+        
+        if(!isValid(temp)) {
             res.status(400).send({status: false, message: 'Name is required'})
             return
         }
 
+        else{
+            // in case there is a spacing in collge abbreviation 
 
-        if(!isValid(fullName)) {
-            res.status(400).send({status: false, message: 'fullName is required'})
-            return
+            let properspacing = temp.replace(/\s+/g, '') // this will remove all spacing by recursively/ constinously calling regex untill there is no space in b/w
+
+            //in case the name is not in lower case 
+            Name = properspacing.toLowerCase()
+
+            requestBody.name = Name
+            if(!isValidRequestBody(requestBody)) {
+                res.status(400).send({status: false, message: 'Invalid request parameters. Please provide college details'})
+                return
+            }
+
+            // Extract params
+            const {name, fullName, logo } = requestBody;
+
+
+            // Validation starts
+            // WE ALREADY VALIDATED THE PRESENCE OF NAME ABOVE IN LINE 19
+            // if(!isValid(name)) {
+            //     res.status(400).send({status: false, message: 'Name is required'})
+            //     return
+            // }
+
+
+            if(!isValid(fullName)) {
+                res.status(400).send({status: false, message: 'fullName is required'})
+                return
+            }
+
+
+            if(!isValid(logo)) {
+                res.status(400).send({status: false, message: 'logo is required'})
+                return
+            }
+            
+            const collegeData = {
+                name,
+                fullName,
+                logo
+            }
+            
+
+            let data = await collegeModel.create(collegeData);
+            res.status(201).send({ status: true, data: data })
+
         }
 
-
-        if(!isValid(logo)) {
-            res.status(400).send({status: false, message: 'logo is required'})
-            return
-        }
-
-        const collegeData = {
-            name,
-            fullName,
-            logo
-        }
-        
-
-        let data = await collegeModel.create(collegeData);
-        res.status(201).send({ status: true, data: data })
     }
     catch (err) {
         res.status(400).send({ msg: err.message })
